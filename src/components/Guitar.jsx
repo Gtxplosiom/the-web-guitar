3b1697d8282e76a18596173ef8bc5dab7b8dfad2
Gtxplosion <tryzlersean05@gmail.com>
2025-07-30 02:44:04 +0800

    yeah yoh

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -23,10 +23,7 @@ function Guitar() {
     return (
         <div className="guitar-container">
             <div className="guitar-image-wrapper">
-                <img
-                    src="/acoustic-guitar-portion.svg"s
-                    alt="Acoustic Guitar"
-                    draggable="false"
+                <img src="/acoustic-guitar-portion.svg" alt="Acoustic Guitar" draggable="false"
                 />
             </div>
         </div>
