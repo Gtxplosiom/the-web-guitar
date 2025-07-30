6166bbd9d274dbefaabc5d5c4d1c24d3137f2072
Gtxplosion <tryzlersean05@gmail.com>
2025-07-30 15:35:17 +0800

    added strings and basic pluck method

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,34 +1,26 @@
 import './Guitar.css';
-import { useEffect } from 'react';
 
-function Guitar() {
-    useEffect(() => {
-        const resizeHandler = () => {
-            const title = document.querySelector('#title');
-            if (!title) return;
+const Guitar = () => {
+    // pamati ko mas guds kun i-papass an id didi?
+    // TODO: apply karplus here
+    const pluck = (string) => {
+        console.log(`plucked ${string}`)
+    }
 
-            const zoom = Math.round(window.devicePixelRatio * 100);
-            const currentSize = parseInt(getComputedStyle(title).fontSize, 10);
-
-            title.style.fontSize =
-                zoom > 100 ? `${currentSize + (zoom % 100)}px` : `${currentSize + (100 % zoom)}px`;
-        };
-
-        window.addEventListener('resize', resizeHandler);
-
-        // a-add kola kay para prevent memory leaks
-        return () => window.removeEventListener('resize', resizeHandler);
-    }, []);
-
+    // TODO: hardcoding here is ugly as f**k. make string hitbox "magnetic"
     return (
         <div className="guitar-container">
             <div className="guitar-image-wrapper">
                 <img src="/acoustic-guitar-portion.svg" alt="Acoustic Guitar" draggable="false"/>
+                <div className="guitar-string" id="low E" style={{ top: '45.5%' }} onClick={() => pluck("low E")}></div>
+                <div className="guitar-string" id="A" style={{ top: '47.5%' }} onClick={() => pluck("A")}></div>
+                <div className="guitar-string" id="D" style={{ top: '49.5%' }} onClick={() => pluck("D")}></div>
+                <div className="guitar-string" id="G" style={{ top: '51.5%' }} onClick={() => pluck("G")}></div>
+                <div className="guitar-string" id="B" style={{ top: '53.5%' }} onClick={() => pluck("B")}></div>
+                <div className="guitar-string" id="high E" style={{ top: '55.5%' }} onClick={() => pluck("high E")}></div>
             </div>
         </div>
     );
 }
 
 export default Guitar;
-
-// TODO: Add strings on top of the guitar image
